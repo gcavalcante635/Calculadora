9d430062b450aed94d358886b0c62c26996f913c
gcavalcante635 <guilherme.marinho@acad.ifma.edu.br>
2021-03-21 00:21:40 -0300

    Formulário de soma

--- resources/views/soma.blade.php
+++ resources/views/soma.blade.php
@@ -5,8 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Blinker&display=swap" rel="stylesheet">
+    <style> 
+    Body{
+       font-family: 'Blinker', sans-serif;
+    }
+    </style>
+
 </head>
 <body>
-   <h1>Calculadora</h1> 
-</body>
+     <div class="container">
+     <div class="row mt-2">
+       Cabeçalho
+     </div>
+
+     <div class="row mt-2">
+
+      <div class="row">
+      <h1>Calculadora</h1>
+      </div>
+       <form>
+        <div class="mb-3">
+         <label for="primeiroValor" class="form-label">Primeiro valor</label>
+         <input type="number" class="form-control" id="primeiroValor"> 
+        </div>
+        <div class="mb-3">
+         <label for="segundoValor" class="form-label">Segundo valor</label>
+         <input type="number" class="form-control" id="segundoValor">
+        </div>
+        <button type="submit" class="btn btn-primary">somar</button>
+       </form>
+      </div>
+
+     <div class="row mt-2">
+        Rodapé
+     </div>
+    </div>
+ </body>
 </html>
